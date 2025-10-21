276d5797384f4005a300f0782951cf900648ff18
troncosojose22 <troncosojose22@gmail.com>
2025-10-21 14:28:42 -0400

    Completed mod 1 assessment review

--- 1-pure-impure.js
+++ 1-pure-impure.js
@@ -1,9 +1,12 @@
 const nums = [1, 2, 3]
 
 const doublePure = (arr) => {
-  
+  const newArr = arr.map((x) => x * 2);
+  return newArr;
 };
 
 const doubleImpure = (arr) => {
-  
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] * 2
+  }
 };
